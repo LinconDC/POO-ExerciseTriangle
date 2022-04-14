5d6a9e026a5edf9e895d8bf27b1712e7d3eb6ab7
linco <linco@DESKTOP-VIJS1C2>
2022-04-14 16:46:06 -0300

    New commit - improvement

--- POO-AreaTriangle/src/application/Program.java
+++ POO-AreaTriangle/src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Triangle;
+
 public class Program {
 
 	public static void main(String[] args) {
@@ -10,22 +12,24 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		double xA, xB, xC, yA, yB, yC;
+		Triangle X, Y;
+		X = new Triangle();
+		Y = new Triangle();
 
 		System.out.println("Enter the measure of triangle X: ");
-		xA = sc.nextDouble();
-		xB = sc.nextDouble();
-		xC = sc.nextDouble();
+		X.a = sc.nextDouble();
+		X.b = sc.nextDouble();
+		X.c = sc.nextDouble();
 		System.out.println("Enter the measure of triangle Y: ");
-		yA = sc.nextDouble();
-		yB = sc.nextDouble();
-		yC = sc.nextDouble();
+		Y.a = sc.nextDouble();
+		Y.b = sc.nextDouble();
+		Y.c = sc.nextDouble();
 
-		double p = (xA + xB + xC) / 2.0;
-		double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+		double p = (X.a + X.b + X.c) / 2.0;
+		double areaX = Math.sqrt(p * (p - X.a) * (p - X.b) * (p - X.c));
 
-		p = (yA + yB + yC) / 2.0;
-		double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+		p = (Y.a + Y.b + Y.c) / 2.0;
+		double areaY = Math.sqrt(p * (p - Y.a) * (p - Y.b) * (p - Y.c));
 
 		System.out.printf("Triangle X area: %.4f%n", areaX);
 		System.out.printf("Triangle Y area: %.4f%n", areaY);
